d44408b7798d3ed4a5fd585d79da1437d9b21a5d
Matt <mrehberg@mines.edu>
2023-04-02 17:35:20 -0700

    almost done

---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction
+
+This repo is meant to digitize the data within the Lunar Compendium. It looks at all of the sample data `https://www.lpi.usra.edu/lunar/samples/#catalogues,` on the PDFs of this website. It uses a PDF scraper to take the data within the first `Table 1` which contains the information on sample characteristics. It also pulls in the api sample data from `https://curator.jsc.nasa.gov/rest/lunarapi/samples/sampledetails/`. This is all combined into the file `ouputs\final_out.csv`. 
+
+-Matthew Rehberg
--- src/parse_pdfs.py
+++ src/parse_pdfs.py
@@ -16,46 +16,92 @@ import io
 import requests
 global cwd
 import os 
+import pickle
+import glob
 
 def getDataTables(df_samples):
-
-    tableCols = ['SiO2',
-                 'TiO2', 
-                 'Al2O3',
-                 'FeO',
-                 'MnO',
-                 'MgO',
-                 'CaO',
-                 'Na2O',
-                 'K2O',
-                 'P2O5',
-                 'S']
-    
-    df_samples = df_samples.reindex(columns = df_samples.columns.tolist() + tableCols).fillna('')
-    for row in range(len(df_samples)):
+    raw = dict()
+    i=0
+    for row in range(750, len(df_samples)):
+        i+=1
         pageNum = int(df_samples['numPages'][row])
         pdfFile = df_samples['compendiumUrl'][row]
+        sampleID = str(df_samples['id'][row])
         if pageNum > 1:
+            stop_searching = False
             for page in range(1, pageNum+1):
                 try:
-                    tables = camelot.read_pdf(pdfFile, pages = str(page), table_regions=['0,300,800,500'])
-                    table = tables[0].df
-                    if 'SiO2' in table:
-                        #then do cool stuff
-                        
+                    tables = camelot.read_pdf(pdfFile, pages = str(page), table_areas=['60,750,300,70'], flavor='stream')
+                    table_df = tables[0].df
+                    if table_df.iloc[0].str.contains('Table 1').sum() > 0:
+                        print(i)
+                        raw.update({sampleID : table_df})
+                        stop_searching = True
                         #and stop looking for pages
                         break
                 except:
+                    print('error' + sampleID)
                     continue    
+            # try the other side of the page
+            if stop_searching == False:
+                for page in range(1, pageNum+1):
+                    try:
+                        tables = camelot.read_pdf(pdfFile, pages = str(page), table_areas=['300,750,600,70'], flavor='stream')
+                        table_df = tables[0].df
+                        if table_df.iloc[0].str.contains('Table 1').sum() > 0:
+                            print(i+'_op')
+                            raw.update({sampleID : table_df})
+                            #and stop looking for pages
+                            break
+                    except:
+                        print('error' + sampleID)
+                        continue  
+    return raw
 
 
+def loadPickles():
+    pickles = glob.glob(cwd+'outputs\*.pickle')
+    raw = dict()
+    for pick in pickles:
+        with (open(pick, "rb")) as openfile:
+            raw.update(pickle.load(openfile))
+    return raw
 
 
-
-
-
-
+def parseDF(raw, df_samples):
+    outputs = df_samples.copy()
+    for sampleID in raw.keys():
+        #raw key
+        df = raw[sampleID].copy()
+        df['mineralVals'] = [[] for _ in range(len(df))]
+        for col in df.columns:
+            if df[col].str.contains('SiO2').sum() > 0:
+                #this is the column with the mineral names
+                mineralCol = col
+                df[col] = df[col].str.replace(' %','')
+                df['mineralName'] = df[col]
+            elif col != 'mineralVals':
+                dig_col = str(col)+'_dig'
+                df[dig_col] = df[col].str.findall(r'(\d+\.*\d*)').fillna('')
+                df['mineralVals'] = df['mineralVals'] + df[dig_col]
+        df = df[['mineralName', 'mineralVals']]
+        df = df.T
+        df.columns = df.iloc[0]
+        df = df[1:]
+        df['id'] = sampleID
+        outputs = pd.merge(outputs, df, on='id', how='left')
+    return outputs
+    
 
 if __name__=="__main__":
     cwd = os.path.dirname(os.path.realpath(__file__)).replace('src','')
     df_samples = pd.read_csv(cwd+r'outputs\out_prepare.csv')
+    pullData = False
+    if pullData:
+        raw = getDataTables(df_samples)
+        #save raw data as a backstop
+        with open(r'C:\Users\HelloWorld\Documents\_git_code\lunarSoil\outputs\raw_data_750_1000.pickle', 'wb') as f:
+            pickle.dump(raw, f)
+    raw = loadPickles()
+    outputs = parseDF(raw, df_samples)
+    outputs.to_csv(cwd + r'ouputs\final_out.csv')
